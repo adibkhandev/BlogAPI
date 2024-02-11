25a9bad8592953cf4b6a3d6fdc93086adea34bc6
adibkhandev <adibkhaan001@gmail.com>
2024-02-11 07:08:36 +0600

    Compress send

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose')
+const ffmpegStatic = require('ffmpeg-static');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs')
+ffmpeg.setFfmpegPath(ffmpegStatic);
 //JSON
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -25,4 +29,32 @@ app.use('/video',videoRoutes)
 //API
 app.listen(3000,()=>console.log('hi'))
 app.use('/images',express.static(__dirname + '/routes/uploads/images'))
-app.use('/videos',express.static(__dirname + '/routes/uploads/videos'))
+// app.use('/videos',express.static(__dirname + '/routes/uploads/videos'))
+app.get('/videos/:filename',(req,res)=>{
+    const filename = req.params.filename
+    try {
+               
+        const inputVid = './routes/uploads/videos/' + filename.replace('mp4','avi')
+        const outputVid = './routes/uploads/videos/' + filename
+        ffmpeg(inputVid)
+           .format('mp4')
+           .on('error', (err) => console.error('Error:', err))
+           .on('end', () =>{
+               console.log('Conversion done!')
+               res.sendFile(outputVid, { root: __dirname })
+               res.on('finish', () => {
+                // Delete the file after sending
+                fs.unlink(outputVid, (unlinkErr) => {
+                  if (unlinkErr) {
+                    console.error('Error deleting file:', unlinkErr);
+                  } else {
+                    console.log('File was deleted');
+                  }
+                });
+              });
+            })
+           .save(outputVid, { end: true })
+   } catch{
+        console.log('error in ffmpeg')
+   }
+})
